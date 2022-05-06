d8f0cc03a377124982227528aa39841c6a69ba02
Jordan Silke <51223540+jsilke@users.noreply.github.com>
2022-05-06 12:49:54 -0400

    Fix CommunitySpider url. Enable image pipeline. Update .gitignore

--- rossimages/spiders/community.py
+++ rossimages/spiders/community.py
@@ -4,10 +4,15 @@ import scrapy
 class CommunitySpider(scrapy.Spider):
     name = "community"
     allowed_domains = ["www.twoinchbrush.com"]
-    start_urls = ["http://www.twoinchbrush.com/"]
+    start_urls = ["https://www.twoinchbrush.com/fanpaintings"]
 
     def parse(self, response):
-        filename = "ross_community.html"
+        filename = "community.html"
         with open(filename, "wb") as f:
             f.write(response.body)
         self.log(f"Saved file {filename}")
+
+        # Recursively crawl the next page and parse the images.
+        # yield from response.follow_all(
+        #     xpath="ul[@class='pagination']//a[@rel='next']", callback=self.parse
+        # )
