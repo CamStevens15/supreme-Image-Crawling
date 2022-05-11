a9261b9ee0489f1771f7bc886a802cedad6653d5
Jordan Silke <51223540+jsilke@users.noreply.github.com>
2022-05-11 12:34:49 -0400

    Modify RossSpider to scrape all required images.

--- rossimages/spiders/ross.py
+++ rossimages/spiders/ross.py
@@ -3,16 +3,23 @@ from rossimages.items import ImageItem
 
 
 class RossSpider(scrapy.Spider):
+    """This spider crawls original Ross paintings as well as fan reproductions and save them to a directory."""
     name = "ross"
     allowed_domains = ["www.twoinchbrush.com"]
-    start_urls = ["https://www.twoinchbrush.com/all-paintings"]
+    start_urls = [
+        "https://www.twoinchbrush.com/all-paintings",
+        "https://www.twoinchbrush.com/fanpaintings",
+    ]
 
     def parse(self, response):
         URL_PREFIX = "https://www.twoinchbrush.com"
-        for source in response.xpath(
-            "//a[not(@target='_blank')]//div[@class='progressive replace']/@data-img"
-        ).getall():
-            yield ImageItem(image_urls=[f"{URL_PREFIX}{source}"])
+        image_urls = [
+            f"{URL_PREFIX}{source}"
+            for source in response.xpath(
+                "//a[not(@target='_blank')]//div[@class='progressive replace']/@data-img"
+            ).getall()
+        ]
+        yield ImageItem(image_urls=image_urls)
 
         # Recursively crawl the next page and parse the images.
         yield from response.follow_all(
